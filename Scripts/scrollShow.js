7ba838d8e5a5bc6044e3243ec9ee640a9ee9b603
Carloscastro16 <andycastro.001016@gmail.com>
2022-09-24 01:52:50 -0500

    Version 1.1.0
    Se añadieron dos secciones más que funcionan correctamente, el blur de la segunda sección no se realiza con transicion.

---
+++ Scripts/scrollShow.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", reveal)
+function reveal(){
+    var reveals = document.querySelectorAll(".revelado")
+
+    for(let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        }else {
+            reveals[i].classList.remove("active");
+        }
+    }
+
+}
